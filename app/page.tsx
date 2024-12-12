5271c614a97b4385d3ab885253235aad571ff681
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2024-12-12 17:16:32 +0630

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ type Props = {
     title: string;
 };
 async function getBlogPosts(): Promise<Props[]> {
-    try{
+    try {
         const data = await client.get({
             endpoint: "blogs",
             queries: {
@@ -15,28 +15,26 @@ async function getBlogPosts(): Promise<Props[]> {
         });
         console.log("HI");
         return data.contents;
-    }catch(error: any)
-    {
-        console.log("Error fetching",error.message, error);
+    } catch (error: any) {
+        console.log("Error fetching", error.message, error);
         return [];
     }
 }
 export default async function Home() {
     const posts = await getBlogPosts();
+
+    console.log({ posts });
+
     return (
         <div>
             <ul>
-                {
-                    posts.map((post) => {
-                        return (
-                            <li key={post.id}>
-                                <Link href={`/blogs/${post.id}`}>
-                                    {post.title}
-                                </Link>
-                            </li>
-                        )
-                    })
-                }
+                {posts.map((post) => {
+                    return (
+                        <li key={post.id}>
+                            <Link href={`/blogs/${post.id}`}>{post.title}</Link>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     );
