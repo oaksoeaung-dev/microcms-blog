ef356cc95b303821e7868fe9f019d85ae4410bb6
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2024-12-12 17:08:46 +0630

    Update env value

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -4,11 +4,11 @@ if (!process.env.MICROCMS_SERVICE_DOMAIN) {
   throw new Error('MICROCMS_SERVICE_DOMAIN is required');
 }
 
-if (!process.env.MICROCMS_API_KEY) {
+if (!process.env.NEXT_PUBLIC_MICROCMS_API_KEY) {
   throw new Error('MICROCMS_API_KEY is required');
 }
 
 export const client = createClient({
   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: process.env.MICROCMS_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
 });
